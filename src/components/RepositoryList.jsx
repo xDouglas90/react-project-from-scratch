ca242c4cc0dfd91d862bca5c923d9930613fd771
xDouglas90 <xdouglas90@gmail.com>
2021-12-03 13:10:31 -0300

    Created basic functional component.

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+const repositoryName = "unform2";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
